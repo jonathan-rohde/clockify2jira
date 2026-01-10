e46445a3101d50226d2146c4250874fab3585e28
Jonathan Rohde <johnny@parallel-bits.de>
2026-01-10 09:52:09 +0100

    cleanup dependencies

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,6 @@ plugins {
     id("org.springframework.boot") version "4.0.1"
     id("io.spring.dependency-management") version "1.1.7"
     kotlin("plugin.spring") version "2.3.0"
-    `maven-publish`
 }
 
 group = "clockify2jira"
