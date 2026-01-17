03a01ad83a5e219d034b74f0bc4d42b601837b9f
Jonathan Rohde <165899591+jonathan-rohde@users.noreply.github.com>
2026-01-17 21:30:42 +0100

    ensure unique worklogs (#14)

--- src/main/kotlin/clockify2jira/jira/JiraService.kt
+++ src/main/kotlin/clockify2jira/jira/JiraService.kt
@@ -10,16 +10,30 @@ import kotlin.time.Duration
 
 interface JiraService {
     fun addWorklog(key: String, start: OffsetDateTime, worklog: Duration)
+    fun hasWorklogs(key: String, start: OffsetDateTime, duration: Duration): Boolean
 }
 
 @Service
 class JiraServiceImpl(
-    private val issueWorklogsApi: IssueWorklogsApi
+    private val issueWorklogsApi: IssueWorklogsApi,
+    private val jiraConfig: JiraConfigProperties
 ) : JiraService {
     private val formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSZ")
 
     override fun addWorklog(key: String, start: OffsetDateTime, worklog: Duration) {
         issueWorklogsApi.addWorklog(key, worklog.toWorklog(key, start))
+    }
+
+    override fun hasWorklogs(key: String, start: OffsetDateTime, duration: Duration): Boolean {
+        val worklogs = issueWorklogsApi.getIssueWorklog(
+            issueIdOrKey = key,
+            startedAfter = (start.toEpochSecond() * 1000) - 1,
+            startedBefore = (start.toEpochSecond() * 1000) + 1
+        ).worklogs ?: emptyList()
+
+        return worklogs.filter { it.author?.accountId == jiraConfig.accountId }
+            .filter { it.timeSpentSeconds == duration.inWholeSeconds }
+            .any { it.started == formatter.format(start) }
     }
 
     private fun Duration.toWorklog(key: String, start: OffsetDateTime): Worklog {
