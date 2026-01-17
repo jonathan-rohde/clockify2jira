c05edce38c24f9cb4b629e5ddbbd788869474fb6
Jonathan Rohde <johnny@parallel-bits.de>
2026-01-17 21:31:10 +0100

    release 1.0.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -12,7 +12,7 @@ plugins {
 }
 
 group = "clockify2jira"
-version = "1.0.3"
+version = "1.0.4"
 
 kotlin.sourceSets["main"].kotlin.srcDir(layout.buildDirectory.dir("generated/openapi/src/main/kotlin"))
 
